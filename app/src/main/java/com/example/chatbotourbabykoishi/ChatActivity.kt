2a849359fc38acb849d2af76a17138e762e5ad17
kumankoishi.py <7iggif@naver.com>
2020-09-12 02:52:59 +0900

    Completed implementing send and receive chat messages

--- app/src/main/java/com/example/chatbotourbabykoishi/ChatActivity.kt
+++ app/src/main/java/com/example/chatbotourbabykoishi/ChatActivity.kt
@@ -13,7 +13,6 @@ import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-
 class ChatActivity : AppCompatActivity() {
 
     var contentText: EditText? = null
@@ -34,6 +33,10 @@ class ChatActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        // [Initialize Koishi Chatbot]
+        KoishiChatBot.initAssistant(this)
+        // [DONE Initialize Koishi Chatbot]
+
         // [Get references]
         contentText = findViewById<EditText>(R.id.chatContent)
         listView = findViewById<RecyclerView>(R.id.recyclerView)
@@ -51,7 +54,7 @@ class ChatActivity : AppCompatActivity() {
         listView!!.layoutManager = LinearLayoutManager(this)
         listView!!.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
             if (bottom < oldBottom) {
-                listView!!.postDelayed(Runnable() {
+                listView!!.postDelayed( {
                     if(messageItems.size != 0) {
                         listView!!.smoothScrollToPosition(messageItems.size - 1)
                     }
@@ -110,7 +113,20 @@ class ChatActivity : AppCompatActivity() {
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
 
-        // [NEXT] Get Koishi's response by using Dialogflow system
-        //
+        koishiResponse(cont)
+    }
+
+    private fun koishiResponse(msg: String) {
+        // Get Dialogflow intent
+        val result = KoishiChatBot.talkToKoishi(123, msg)
+        val responseMsg = result.fulfillmentText
+        Log.d(MainActivity.TAG + "ChatBotResponse", responseMsg)
+
+        val uid = "${R.string.koishiChattingId}"
+        val time: Long = System.currentTimeMillis()
+
+        val msgToSend = ChatData(time, uid, responseMsg)
+        chatRef!!.push().setValue(msgToSend)
+        contentText!!.text.clear()
     }
 }
---
+++ app/src/main/java/com/example/chatbotourbabykoishi/KoishiChatBot.kt
@@ -0,0 +1,84 @@
+package com.example.chatbotourbabykoishi
+
+import android.content.Context
+import android.util.Log
+import com.google.api.gax.core.FixedCredentialsProvider
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.cloud.dialogflow.v2.*
+import com.google.protobuf.Struct
+import com.google.protobuf.Value
+import java.io.InputStream
+
+object KoishiChatBot {
+    private var client: SessionsClient? = null
+    private var session: SessionName? = null
+    private const val LANGUAGE_CODE = "en-US"
+
+    private var stream: InputStream? = null
+    private var credentials: GoogleCredentials? = null
+    private var projectId: String? = null
+
+    private var settingsBuilder: SessionsSettings.Builder? = null
+    private var sessionsSettings: SessionsSettings? = null
+
+    fun initAssistant(context: Context) {
+        try {
+            stream = context.resources.openRawResource(R.raw.ourbabykoishi)
+            credentials = GoogleCredentials.fromStream(stream)
+            projectId = (credentials as ServiceAccountCredentials).projectId
+
+            settingsBuilder = SessionsSettings.newBuilder()
+            sessionsSettings = settingsBuilder!!.setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build()
+
+            Log.d(MainActivity.TAG + "Dialogflow", settingsBuilder.toString())
+            Log.d(MainActivity.TAG + "Dialogflow", sessionsSettings.toString())
+
+            client = SessionsClient.create(sessionsSettings)
+
+            Log.d(MainActivity.TAG + "Dialogflow", "Initialization complete.")
+        } catch (e: Exception) {
+            Log.e(MainActivity.TAG + "Dialogflow", "Dialogflow system failed.")
+            e.printStackTrace()
+        }
+    }
+
+    fun talkToKoishi(somePayload: Int, sessionText: String): QueryResult {
+        session = SessionName.of(projectId, sessionText)
+        return detectIntentText(somePayload, sessionText)
+    }
+
+    @Throws(Exception::class)
+    fun detectIntentText(somePayload: Int, text: String): QueryResult {
+        if(client == null || session == null){
+            throw Exception("Error: no dialogflow client")
+        }
+
+        // Set the text (input) and language code (en) for the query
+        val textInput = TextInput.newBuilder().setText(text).setLanguageCode(LANGUAGE_CODE)
+
+        // Build the query with the TextInput
+        val queryInput = QueryInput.newBuilder().setText(textInput).build()
+
+        // Save received payload into a protobuf Struct
+        val payLoad = Value.newBuilder().setNumberValue(somePayload.toDouble()).build()
+        val struct = Struct.newBuilder()
+        struct.putFields("somePayload", payLoad)
+
+        // Set queryParameters
+        val queryParameters = QueryParameters.newBuilder().setPayload(struct).build()
+
+        // Build the request
+        val request = DetectIntentRequest.newBuilder()
+            .setSession(session.toString())
+            .setQueryInput(queryInput)
+            .setQueryParams(queryParameters)
+            .build()
+
+        // Performs the detect intent request
+        val response = client!!.detectIntent(request)
+
+        // Display the query result
+        return response.queryResult
+    }
+}
